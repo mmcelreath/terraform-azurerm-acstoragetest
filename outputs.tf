f5c4cc596df3282cc8bfa23b56ea3ef6cbf0ad02
Matt McElreath <matt.mcelreath@gmail.com>
2022-07-17 14:50:39 -0400

    Initial Commit

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "storage_account_id" {
+  value = azurerm_storage_account.sa.id
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name of the Resource Group to create."
+  type = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the Storage Account to create."
+  type = string
+}
+
+variable "location" {
+  description = "The location for the resources"
+  type = string
+}
